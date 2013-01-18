20aed851ebf237d1dbb4abc05b650af267c107a9
Pierros Papadeas <ppapadeas@gmail.com>
2013-01-18 23:37:03 +0200

    CSS beautification and footer addition

--- index.php
+++ index.php
@@ -1,24 +1,33 @@
-<html> 
+<html>
 <head>
-  <meta charset="utf-8" />
-  <title>What Is My IP</title>
+	<meta charset="utf-8" />
+	<title>ipwh.at</title>
 
-<link href='http://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
-<link href="css/ip.css" rel='stylesheet' type='text/css'>
-
+	<link href="css/ip.css" rel='stylesheet' type='text/css'>
 </head>
+
 <body>
 
-<div id="ip" >
-<?php
-	echo $_SERVER["REMOTE_ADDR"] ."<br>" . gethostbyaddr( $_SERVER['REMOTE_ADDR'] ) ;
-?>
-</div>
-
-<p align=right>Click here to <a href="#" id="install_app"> <b> install </b></a></p>
+	<div id="container">
+		<div id="header">
+			ipwh.at
+		</div>
+		
+		<div id="ip" >
+			<?php echo $_SERVER["REMOTE_ADDR"] ."<br>" . gethostbyaddr( $_SERVER['REMOTE_ADDR'] ) ;?>
+		</div>
+		
+		<div id="footer">
+			<hr>
+			Open Web App to display your IP and Hostname
+			<br>by <a href="http://ebalaskas.gr/">Ebal</a>
+			 - <a href="https://github.com/hsgr/ipwh.at">Fork me on Github</a>
+			 - <a href="#" id="install_app">Click here to install</a>
+		</div>
+	</div>
 
 </body>
 
-<script language="JavaScript" type="text/javascript" src="js/install.js"> </script>
+<script type="text/javascript" src="js/install.js"></script>
 
 </html>
--- js/install.js
+++ js/install.js
@@ -1,5 +1,5 @@
 document.getElementById("install_app").onclick = function(){
-        var request = window.navigator.mozApps.install('http://ebalaskas.gr/ip/manifest/ip.webapp');
+        var request = window.navigator.mozApps.install('http://ipwh.at/ip.webapp');
         request.onsuccess = function () {
                 // Save the App object that is returned
                 var appRecord = this.result;
